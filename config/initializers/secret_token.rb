0957ce2e7a18c77c9c3634960e7513486c66cff3
Paul Sutcliffe <paul@kosmyka.com>
2013-08-07 04:49:24 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kosmyka::Application.config.secret_token = 'd970c6f4a626620bd21058d393c3c3daa5ca6c8c746d85762060372e704b1f8d06f672aaae9ffdb54b6c89fd3cb9713d49b19282aedeb63d5f4fdd376ce40693'
