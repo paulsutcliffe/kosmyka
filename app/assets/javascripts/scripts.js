2271443a0c082160e849fcde8745354798fa6ad6
Ricardo <marcorcp1@hotmail.com>
2013-08-27 23:57:07 -0500

    ajustes del parallax

--- app/assets/javascripts/scripts.js
+++ app/assets/javascripts/scripts.js
@@ -1,3 +1,15 @@
 $(document).ready(function() {
-
+  parallax_margin();
 });
+
+$(window).resize(function() {
+  parallax_margin();
+});
+
+
+function parallax_margin() {
+  h_height = $('.navbar-fluid-top').height();
+  p_height = $('#parallaxContainer').height();
+  content_margin = p_height - h_height;
+  $('#content-middle').css('margin-top', content_margin);
+}
