470b2a04b109e0b995d5e6394769ca0fa2da7724
Paul Sutcliffe <paul@kosmyka.com>
2013-08-27 19:05:33 -0500

    Implementado carrierwave para projects

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,5 @@
 class Project < ActiveRecord::Base
-  attr_accessible :areas, :client, :description, :launched, :name, :tecnologies, :type, :url
+  attr_accessible :areas, :client, :description, :launched, :name, :tecnologies, :type, :url, :image
+
+  mount_uploader :image, ImageUploader
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130816231409) do
+ActiveRecord::Schema.define(:version => 20130827233428) do
 
   create_table "contacts", :force => true do |t|
     t.string   "name"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(:version => 20130816231409) do
     t.string   "type"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
+    t.string   "image"
   end
 
 end
