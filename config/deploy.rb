57152fb6119a5713dafe1552c10286a10f086fdb
Paul Sutcliffe <paul@kosmyka.com>
2014-02-14 12:13:13 -0500

    update

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 require "bundler/capistrano"
 require "rvm/capistrano"
 
-set :rvm_ruby_string, '1.9.3'
+set :rvm_ruby_string, '2.0'
 set :rvm_type, :user  # Don't use system-wide RVM
 
 server "rubyonrailsperu.com", :web, :app, :db, primary: true
